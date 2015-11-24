fa0ebbf0c3b095be06f9faa53d0d5dac0ebdc9bc
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-24 23:45:03 +0100

    optimize code

--- views/alt_arten_mit_synonymen/map.js
+++ views/alt_arten_mit_synonymen/map.js
@@ -3,7 +3,6 @@
 function (doc) {
   'use strict'
 
-  var _ = require('views/lib/underscore')
   var findStandardTaxonomyInDoc = require('views/lib/findStandardTaxonomyInDoc')
 
   if (doc.Typ && doc.Typ === 'Objekt' && doc.Gruppe && doc.Taxonomien) {
@@ -12,23 +11,23 @@ function (doc) {
       // sicherstellen, dass Taxonomie-ID existiert
       if (standardtaxonomie && standardtaxonomie.Eigenschaften && standardtaxonomie.Eigenschaften['Taxonomie ID']) {
         // sicherstellen, dass GIS-Layer und Betrachtungsdistanz existieren
-        if (doc.Eigenschaftensammlungen && doc.Eigenschaftensammlungen.length > 0) {
+        if (doc.Eigenschaftensammlungen) {
           // durch alle Eigenschaftensammlungen loopen
-          _.each(doc.Eigenschaftensammlungen, function (datensammlung) {
+          doc.Eigenschaftensammlungen.forEach(function (datensammlung) {
             if (datensammlung.Name && datensammlung.Name === 'ZH GIS' && datensammlung.Eigenschaften && datensammlung.Eigenschaften['GIS-Layer'] && datensammlung.Eigenschaften['Betrachtungsdistanz (m)']) {
               // ok, alle benötigten Felder sind vorhanden
               // erst mal das eigene Dokument senden
               // der zweite key markiert, dass dies das Original ist
               emit([doc._id, 1])
-              if (doc.Beziehungssammlungen && doc.Beziehungssammlungen.length > 0) {
+              if (doc.Beziehungssammlungen) {
                 // durch alle Beziehungssammlungen loopen
-                _.each(doc.Beziehungssammlungen, function (beziehungssammlung) {
+                doc.Beziehungssammlungen.forEach(function (beziehungssammlung) {
                   if (beziehungssammlung.Typ && beziehungssammlung.Typ === 'taxonomisch' && beziehungssammlung['Art der Beziehungen'] && beziehungssammlung['Art der Beziehungen'] === 'synonym' && beziehungssammlung.Beziehungen && beziehungssammlung.Beziehungen.length > 0) {
                     // jetzt durch alle synonymen Beziehungen loopen
-                    _.each(beziehungssammlung.Beziehungen, function (beziehung) {
+                    beziehungssammlung.Beziehungen.forEach(function (beziehung) {
                       if (beziehung.Beziehungspartner && beziehung.Beziehungspartner.length > 0) {
                         // durch alle Beziehungspartner der synonymen Beziehungen loopen
-                        _.each(beziehung.Beziehungspartner, function (bez_partner) {
+                        beziehung.Beziehungspartner.forEach(function (bez_partner) {
                           if (bez_partner.GUID) {
                             // veranlassen, dass mit include_docs=true auch das Dokument dieses Synonyms gesendet wird
                             // der zweite key markiert, dass es ein Synonym ist
