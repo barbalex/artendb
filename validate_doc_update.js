a6f1180e540fb2b5f668203b59becb268d7c2dd5
Alexander Gabriel <alex.barbalex@gmail.com>
2015-12-12 20:56:58 +0100

    formulate plan to validate

--- validate_doc_update.js
+++ validate_doc_update.js
@@ -8,6 +8,10 @@ function(newDoc, oldDoc, userCtx, secObj) {
   if (!userCtx.name) throw({forbidden: 'Sie müssen angemeldet sein'})
 
   /**
+   * TODO: if user is admin, wave him through without all the computations that follow
+   */
+
+  /**
    * make sure guids of objects are formatted correctly
    */
   var isGuidFormatCorrect = new RegExp('^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$').test(newDoc._id)
@@ -18,7 +22,7 @@ function(newDoc, oldDoc, userCtx, secObj) {
   }
 
   /**
-   * make sure, objects of gruppe Fauna and Flora have GIS-Layer and Betrachtungsdistanz
+   * make sure objects of gruppe Fauna and Flora have GIS-Layer and Betrachtungsdistanz
    * they need it to be analysed in ALT
    */
   var betrachtungsdistanzMessage = 'Jede Art der Gruppen "Fauna" und "Flora" braucht ein GIS-Layer und eine Betrachtungsdistanz, d.h.:\n- eine Eigenschaft "Eigenschaftensammlungen" (ein Array)\n- darin eine Eigenschaftensammlung (= Objekt) mit den nötigen Attributen\n\nSiehe diesen Link:\nhttps://gist.github.com/barbalex/d98aad9a96978b137dde\nbzw. dieses vollständige Beispiel:\nhttps://gist.github.com/barbalex/9836162'
@@ -37,17 +41,29 @@ function(newDoc, oldDoc, userCtx, secObj) {
   }
 
   /**
-   * if Eigenschaftensammlungen, Beziehungssammlungen or Taxonomien were changed
+   * if doc, Eigenschaftensammlungen, Beziehungssammlungen or Taxonomien were changed
    * user needs the correct role
    *
    * This is the plan:
-   * - build array of collections that were added in newDoc
-   *   - loop them and check if user has correct role to add
-   * - build array of collections that were removed in newDoc
-   *   - loop them and check if user has correct role to remove
-   * - build array of all other collections
-   *   - loop them and check if collection was changed
-   *     - if true, check if user has correct role to change it
-   * - repeat for pcs, rcs, taxonomies
+   * 
+   * - check if doc is new. If true:
+   *   - exists "Organisation mit Schreibrecht"?
+   *   - does user have correct role to create it?
+   *
+   * - check if doc was deleted (newDoc._deleted). If true:
+   *   - does user have correct role to delete it?
+   *
+   * repeat for pcs, rcs, taxonomies:
+   * - build array of collections that were added. Loop them and check:
+   *   - exists "Organisation mit Schreibrecht"?
+   *   - does user have correct role to add it?
+   * 
+   * - build array of collections that were removed. Loop them and check:
+   *   - does user have correct role to remove it?
+   * 
+   * - build array of all other collections. Loop them and check:
+   *   - was the collection changed? If true:
+   *     - does user have correct role to change it?
+   *
    */
 }
