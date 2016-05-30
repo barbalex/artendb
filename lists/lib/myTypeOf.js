39e419c63f8d1a2f70ea75da517fa89be096fb99
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-30 12:13:07 +0200

    optimize code

--- lists/lib/myTypeOf.js
+++ lists/lib/myTypeOf.js
@@ -1,19 +1,29 @@
+/* eslint ecmaVersion: 5 */
 // Hilfsfunktion, die typeof ersetzt und ergänzt
 // typeof gibt bei input-Feldern immer String zurück!
 
 'use strict'
 
-module.exports = function (wert) {
-  if (typeof wert === 'boolean') { return 'boolean' }
-  if (parseInt(wert, 10) && parseFloat(wert) && parseInt(wert, 10) !== parseFloat(wert) && parseInt(wert, 10) == wert) { // eslint-disable-line eqeqeq
+module.exports = function(wert) {
+  if (wert === undefined) return 'undefined'
+  if (typeof wert === 'boolean') return 'boolean'
+  if (
+    parseInt(wert, 10) &&
+    parseFloat(wert) &&
+    parseInt(wert, 10) !== parseFloat(wert) &&
+    parseInt(wert, 10) == wert  // eslint-disable-line eqeqeq
+  ) {
     return 'float'
   }
   // verhindern, dass führende Nullen abgeschnitten werden
-  if ((parseInt(wert, 10) == wert && wert.toString().length === Math.ceil(parseInt(wert, 10) / 10)) || wert == '0') { // eslint-disable-line eqeqeq
+  if (
+    (
+      parseInt(wert, 10) == wert &&  // eslint-disable-line eqeqeq
+      wert.toString().length === Math.ceil(parseInt(wert, 10) / 10)
+    ) ||
+    wert == '0'  // eslint-disable-line eqeqeq
+  ) {
     return 'integer'
   }
-  if (typeof wert === 'object') { return 'object' }
-  if (typeof wert === 'string') { return 'string' }
-  if (wert === undefined) { return 'undefined' }
-  if (typeof wert === 'function') { return 'function' }
+  return typeof wert
 }
