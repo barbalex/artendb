406a3d58dfa1b24096916e569f0b9c8f1ff943ec
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-30 12:13:28 +0200

    optimize code

--- views/lib/findStandardTaxonomyInDoc.js
+++ views/lib/findStandardTaxonomyInDoc.js
@@ -1,10 +1,13 @@
+/* eslint ecmaVersion: 5 */
 'use strict'
 
-module.exports = function (doc) {
+module.exports = function(doc) {
   var standardtaxonomie = null
   if (doc.Taxonomien) {
-    doc.Taxonomien.forEach(function (taxonomy) {
-      if (taxonomy.Standardtaxonomie) standardtaxonomie = taxonomy
+    doc.Taxonomien.forEach(function(taxonomy) {
+      if (taxonomy.Standardtaxonomie) {
+        standardtaxonomie = taxonomy
+      }
     })
   }
   return standardtaxonomie
