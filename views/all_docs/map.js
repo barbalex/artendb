101805aee583aa70992223802aff1f4529e8e0fd
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-30 12:13:18 +0200

    optimize code

--- views/all_docs/map.js
+++ views/all_docs/map.js
@@ -1,4 +1,5 @@
-﻿function (doc) {
+﻿/* eslint ecmaVersion: 5 */
+function(doc) {
   'use strict'
 
   emit(doc._id)
--- views/alt_arten_mit_synonymen/map.js
+++ views/alt_arten_mit_synonymen/map.js
@@ -1,33 +1,51 @@
-﻿// wählt alle Dokumente, die GIS-Layer und Betrachtungsdistanz enthalten
+﻿/* eslint ecmaVersion: 5 */
+// wählt alle Dokumente, die GIS-Layer und Betrachtungsdistanz enthalten
 // sowie ihre Synonyme
-function (doc) {
+function(doc) {
   'use strict'
 
   var findStandardTaxonomyInDoc = require('views/lib/findStandardTaxonomyInDoc')
+  var _ = require('views/lib/lodash')
 
-  if (doc.Typ && doc.Typ === 'Objekt' && doc.Gruppe && doc.Taxonomien) {
-    if (doc.Gruppe === 'Fauna' || doc.Gruppe === 'Flora') {
+  if (
+    doc.Typ &&
+    doc.Typ === 'Objekt' &&
+    doc.Gruppe &&
+    doc.Taxonomien
+  ) {
+    if (
+      doc.Gruppe === 'Fauna' ||
+      doc.Gruppe === 'Flora'
+    ) {
       var standardtaxonomie = findStandardTaxonomyInDoc(doc)
       // sicherstellen, dass Taxonomie-ID existiert
-      if (standardtaxonomie && standardtaxonomie.Eigenschaften && standardtaxonomie.Eigenschaften['Taxonomie ID']) {
+      if (_.has(standardtaxonomie, ['Eigenschaften', 'Taxonomie ID'])) {
         // sicherstellen, dass GIS-Layer und Betrachtungsdistanz existieren
         if (doc.Eigenschaftensammlungen) {
           // durch alle Eigenschaftensammlungen loopen
-          doc.Eigenschaftensammlungen.forEach(function (datensammlung) {
-            if (datensammlung.Name && datensammlung.Name === 'ZH GIS' && datensammlung.Eigenschaften && datensammlung.Eigenschaften['GIS-Layer'] && datensammlung.Eigenschaften['Betrachtungsdistanz (m)']) {
+          doc.Eigenschaftensammlungen.forEach(function(datensammlung) {
+            if (
+              _.get(datensammlung, 'Name') === 'ZH GIS' &&
+              _.has(datensammlung, ['Eigenschaften', 'GIS-Layer']) &&
+              _.has(datensammlung, ['Eigenschaften', 'Betrachtungsdistanz (m)'])
+            ) {
               // ok, alle benötigten Felder sind vorhanden
               // erst mal das eigene Dokument senden
               // der zweite key markiert, dass dies das Original ist
               emit([doc._id, 1])
               if (doc.Beziehungssammlungen) {
                 // durch alle Beziehungssammlungen loopen
-                doc.Beziehungssammlungen.forEach(function (beziehungssammlung) {
-                  if (beziehungssammlung.Typ && beziehungssammlung.Typ === 'taxonomisch' && beziehungssammlung['Art der Beziehungen'] && beziehungssammlung['Art der Beziehungen'] === 'synonym' && beziehungssammlung.Beziehungen) {
+                doc.Beziehungssammlungen.forEach(function(beziehungssammlung) {
+                  if (
+                    _.get(beziehungssammlung, 'Typ') === 'taxonomisch' &&
+                    _.get(beziehungssammlung, 'Art der Beziehungen') === 'synonym' &&
+                    _.has(beziehungssammlung, 'Beziehungen')
+                  ) {
                     // jetzt durch alle synonymen Beziehungen loopen
-                    beziehungssammlung.Beziehungen.forEach(function (beziehung) {
+                    beziehungssammlung.Beziehungen.forEach(function(beziehung) {
                       if (beziehung.Beziehungspartner) {
                         // durch alle Beziehungspartner der synonymen Beziehungen loopen
-                        beziehung.Beziehungspartner.forEach(function (bezPartner) {
+                        beziehung.Beziehungspartner.forEach(function(bezPartner) {
                           if (bezPartner.GUID) {
                             // veranlassen, dass mit include_docs=true auch das Dokument dieses Synonyms gesendet wird
                             // der zweite key markiert, dass es ein Synonym ist
