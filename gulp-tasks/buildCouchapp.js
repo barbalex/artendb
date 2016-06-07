8418d58d0b43f2fc987608df573986062b33079c
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-07 10:40:54 +0200

    get update to work

--- gulp-tasks/buildCouchapp.js
+++ gulp-tasks/buildCouchapp.js
@@ -7,7 +7,7 @@ const passFile = require('../couchPass.json')
 const userName = passFile.user
 const password = passFile.pass
 
-const request = `couchapp push http://${userName}:${password}@127.0.0.1:5984/ae`
+const request = `couchapp push http://${userName}:${password}@127.0.0.1:5984/artendb`
 // const request = `couchapp push http://${userName}:${password}@46.101.210.208:5984/ae`
 
 console.log('request: ', request)
